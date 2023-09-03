5a5af9eff26bd75a49221f650e14b8a3bda7d1d1
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-09-03 19:00:00 +0500

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AddTodo from "./Component/AddTodo";
 import ListContainer from "./Component/ListContainer";
 
 function App() {
-  const [todos, setTodos] = useState(["hello"]);
+  const [todos, setTodos] = useState([]);
   return (
     <div className="Main-Container flex flex-col items-center justify-center ">
       <h1 className="font-semibold text-4xl mb-10">React Todo Application</h1>
