057d343274fcfbe0de7a4b673019d317034d6383
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-09-05 15:35:53 +0500

    added delete all todos functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "./Component/AddTodo";
 import ListContainer from "./Component/ListContainer";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+  const [todos, setTodos] = useState(["Jeet"]);
   return (
     <div className="Main-Container flex flex-col items-center justify-center ">
       <h1 className="font-semibold text-4xl mb-10">React Todo Application</h1>
       <AddTodo setTodos={setTodos} />
+      <h2 className="text-2xl md:text-4xl font-bold mt-5 text-right">Your All Todos</h2>
       <ListContainer todos={todos} setTodos={setTodos} />
     </div>
   );
--- src/Component/AddTodo.jsx
+++ src/Component/AddTodo.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 function AddTodo({ setTodos }) {
   const [formValue, setFormValue] = React.useState("");
+
+
+  //Method To Handle ADD Functionality
   function addTodoHandler(e) {
     e.preventDefault();
     if (formValue) {
@@ -11,17 +14,23 @@ function AddTodo({ setTodos }) {
       alert("Please Enter Task");
     }
   }
+  //Method To Handle Delete All Todos Functionality
+    function handleDeleteAllTodos(){
+      setTodos([]);
+    }
+
   return (
     <div className="Add-Todo w-full">
-      <form className="flex gap-4 text-black" onSubmit={addTodoHandler}>
+      <form className="flex flex-col md:flex-row gap-4 text-black" onSubmit={addTodoHandler}>
         <input
           type="text"
-          className="p-3 rounded-lg text-lg flex-1"
+          className="p-3 rounded-lg text-lg flex-1 md:w-[400px]"
           onChange={(e) => setFormValue(e.target.value)}
           value={formValue}
           placeholder="Add Task Here..."
         />
         <button className="p-3 rounded-lg  bg-emerald-500">Add Todo</button>
+        <button className="p-3 rounded-lg  bg-red-500" type="button" onClick={handleDeleteAllTodos}>Delete All Todos</button>
       </form>
     </div>
   );
