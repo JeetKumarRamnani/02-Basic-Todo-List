40eeb724d26c1d488a0aa898edb371edf1c3d067
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-09-05 15:21:37 +0500

    added edit functionality

--- src/Component/ListContainer.jsx
+++ src/Component/ListContainer.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 function ListContainer({ todos, setTodos }) {
+
+//Method To Handle Delete Functionality
   function handleDeleteTodo(id) {
     const Todos = [...todos];
     Todos.splice(id, 1);
     setTodos(Todos);
   }
 
+//Method To Handle Edit Functionality
+  function handleEditTodo(id) {
+    const updatedTodo = prompt("Task To Edit", todos[id]);
+    
+    if (updatedTodo !== null) { 
+      setTodos(prevTodos => {
+        const newTodos = [...prevTodos];
+        newTodos[id] = updatedTodo;
+        return newTodos;
+      });
+    }
+  }
+  
+//Method To Capitilize The First Letter Of Userinput String
   function titleCase(str) {
     return str[0].toUpperCase() + str.slice(1);
   }
@@ -21,11 +37,18 @@ function ListContainer({ todos, setTodos }) {
             <h3 className="flex-1 border-2 px-4 py-2 font-semibold">
               {titleCase(task)}
             </h3>
+            
+            <button
+              className="bg-blue-500 rounded-lg shadow-lg px-4 py-2"
+              onClick={() => handleEditTodo(index)}
+            >
+              Edit
+            </button>
             <button
               className="bg-red-500 rounded-lg shadow-lg px-4 py-2"
               onClick={() => handleDeleteTodo(index)}
             >
-              Delete Todo
+              Delete
             </button>
           </li>
         );
